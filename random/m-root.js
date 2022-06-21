f324019e813e9901c1eb669dbb0d10d040b5c486
distray <52904880+distray@users.noreply.github.com>
2022-06-21 00:56:45 -0500

    Create m-root.js

---
+++ random/m-root.js
@@ -0,0 +1,15 @@
+Math.sqrt(number);
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);
+
+const number1 = 4;
+const number2 = 6;
+
+const result1 = Math.sqrt(number1);
+const result2 = Math.sqrt(number2);
+
+console.log(`The square root of ${number1} is ${result1}`);
+console.log(`The square root of ${number2} is ${result2}`);
+console.log(`Confirmed`);
